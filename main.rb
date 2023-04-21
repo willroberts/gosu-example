5b15803eb8ab7368772139611ac2ff4160d35c8a
Will Roberts <willroberts3@protonmail.com>
2023-04-21 14:15:39 -0400

    Replaces background sprite with grass

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ class GameWindow < Gosu::Window
     super WINDOW_WIDTH, WINDOW_HEIGHT, :fullscreen => false
     self.caption = "Example Game"
 
-    @eggplant = Gosu::Image.new("eggplant.png", :tileable => true)
+    @grass = Gosu::Image.new("grass.png", :tileable => true)
   end
 
   # update() runs reliably at 60fps.
@@ -20,7 +20,7 @@ class GameWindow < Gosu::Window
   def draw
     0.step(WINDOW_WIDTH, 40).each do |x|
       0.step(WINDOW_HEIGHT, 40).each do |y|
-        @eggplant.draw(x, y, 0)
+        @grass.draw(x, y, 0)
       end
     end
   end
