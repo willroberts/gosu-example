78c09872f045e00c27e250ed31df109c2ff6568b
Will Roberts <willroberts3@protonmail.com>
2023-04-21 14:26:55 -0400

    Adds character class

---
+++ character.rb
@@ -0,0 +1,45 @@
+class Character
+  def initialize
+    @image = Gosu::Image.new("eggplant.bmp")
+    @x = 0.0
+    @y = 0.0
+    @sprite_size = 40.0
+    @move_distance = 20.0
+  end
+
+  def warp(x, y)
+    @x, @y = x, y
+  end
+
+  def move_up
+    if @y < @move_distance
+      return
+    end
+    @y = @y - @move_distance
+  end
+
+  def move_down
+    if @y > WINDOW_HEIGHT - (@sprite_size + @move_distance)
+      return
+    end
+    @y = @y + @move_distance
+  end
+
+  def move_left
+    if @x < @move_distance
+      return
+    end
+    @x = @x - @move_distance
+  end
+
+  def move_right
+    if @x > WINDOW_WIDTH - (@sprite_size + @move_distance)
+      return
+    end
+    @x = @x + @move_distance
+  end
+
+  def draw
+    @image.draw(@x, @y, 1)
+  end
+end
--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'gosu'
+require './character.rb'
 
 WINDOW_WIDTH = 320
 WINDOW_HEIGHT = 240
@@ -10,10 +11,14 @@ class GameWindow < Gosu::Window
     self.caption = "Example Game"
 
     @grass = Gosu::Image.new("grass.png", :tileable => true)
+
+    @character = Character.new
+    @character.warp(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
   end
 
   # update() runs reliably at 60fps.
   def update
+    handle_input
   end
 
   # draw() runs unreliably at 60fps.
@@ -22,6 +27,25 @@ class GameWindow < Gosu::Window
       0.step(WINDOW_HEIGHT, 40).each do |y|
         @grass.draw(x, y, 0)
       end
+    end
+    @character.draw
+  end
+
+  def handle_input
+    if Gosu.button_down? Gosu::KB_ESCAPE
+      close
+    end
+    if Gosu.button_down? Gosu::KB_UP
+      @character.move_up
+    end
+    if Gosu.button_down? Gosu::KB_DOWN
+      @character.move_down
+    end
+    if Gosu.button_down? Gosu::KB_LEFT
+      @character.move_left
+    end
+    if Gosu.button_down? Gosu::KB_RIGHT
+      @character.move_right
     end
   end
 end
